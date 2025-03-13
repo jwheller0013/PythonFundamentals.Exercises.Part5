22cdb1c9378469a4578b0056f6ce44e1ccdd58b6
jwheller0013 <jwheller0013@yahoo.com>
2025-03-13 16:25:20 -0400

    Anagram 5/7 pass

--- anagram.py
+++ anagram.py
@@ -4,3 +4,15 @@ def is_anagram(first_string: str, second_string: str) -> bool:
     """
     pass  # remove pass statement and implement me
 
+
+    if len(first_string) != len(second_string):
+        return False
+    for i in range(int(len(first_string))):
+        first_string = first_string.lower()
+        second_string = second_string.lower()
+        testing = True
+        if first_string.find(second_string[i]) == 1:
+            testing = True
+        elif first_string.find(second_string[i]) == -1:
+            testing = False
+    return testing
